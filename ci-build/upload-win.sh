57ded6cc0bc709cedfd4c7fc1c57d1339394cd94
KDr2 <zhuo@hexoasis.com>
2019-02-14 16:05:42 +0800

    new deploy depot

--- ci-build/upload-win.sh
+++ ci-build/upload-win.sh
@@ -5,9 +5,9 @@ ARCH=$3
 PDFium_SOURCE_REVIISON=$(git -C ${SOURCE_DIR} rev-parse --short HEAD)
 DATE=$(C:/cygwin64/bin/date +%Y%m%d%H%M)
 
-curl http://cxan.kdr2.com/scripts/pkg-c.php -F token=${CXAN_TOKEN} -F file=@${FILE} \
+curl http://bohr.cheftin.com:8080/pkg-c.php -F token=${CXAN_TOKEN} -F file=@${FILE} \
      -F dest=/pdfium/windows-${ARCH}/pdfium-windows-${ARCH}-${DATE}-${PDFium_SOURCE_REVIISON}.zip
-curl http://cxan.kdr2.com/scripts/pkg-c.php -F token=${CXAN_TOKEN} -F file=@${FILE} \
+curl http://bohr.cheftin.com:8080/pkg-c.php -F token=${CXAN_TOKEN} -F file=@${FILE} \
      -F dest=/pdfium/windows-${ARCH}/pdfium-windows-${ARCH}-latest.zip
 
 curl $NOTIFICATION_MM_WEBHOOK --header "Content-Type: application/json"  --request POST \
@@ -20,19 +20,19 @@ curl $NOTIFICATION_MM_WEBHOOK --header "Content-Type: application/json"  --reque
       "pretext": "PDFium(windows) Build Completed.",
       "text": "A PDFium(windows) building was just Completed. You can download the new package from the links below.",
       "author_name": "Cheftin via Appveyor CI",
-      "author_link": "http://cxan.kdr2.com/pdfium/windows-x64/",
+      "author_link": "http://bohr.cheftin.com:8080/pdfium/windows-x64/",
       "title": "PDFium(windows) Build Completed.",
       "title_link": "https://ci.appveyor.com/project/KDr2/pdfium",
       "fields": [
         {
           "short":true,
           "title":"Permanent Link",
-          "value":"http://cxan.kdr2.com/pdfium/windows-${ARCH}/pdfium-windows-${ARCH}-${DATE}-${PDFium_SOURCE_REVIISON}.zip"
+          "value":"http://bohr.cheftin.com:8080/pdfium/windows-${ARCH}/pdfium-windows-${ARCH}-${DATE}-${PDFium_SOURCE_REVIISON}.zip"
         },
         {
           "short":true,
           "title":"Temporary Alias",
-          "value":"http://cxan.kdr2.com/pdfium/windows-${ARCH}/pdfium-windows-${ARCH}-latest.zip"
+          "value":"http://bohr.cheftin.com:8080/pdfium/windows-${ARCH}/pdfium-windows-${ARCH}-latest.zip"
         }
       ]
     }
